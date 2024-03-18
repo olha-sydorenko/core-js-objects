21674b2145b65dc2900e0effe1d25882561f26e7
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2024-03-18 23:06:53 +0200

    feat: add solutions

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -135,8 +135,15 @@ function makeImmutable(obj) {
  *    makeWord({ a: [0, 1], b: [2, 3], c: [4, 5] }) => 'aabbcc'
  *    makeWord({ H:[0], e: [1], l: [2, 3, 8], o: [4, 6], W:[5], r:[7], d:[9]}) => 'HelloWorld'
  */
-function makeWord(/* lettersObject */) {
-  throw new Error('Not implemented');
+function makeWord(lettersObject) {
+  // throw new Error('Not implemented');
+  const word = [];
+  Object.entries(lettersObject).forEach(([key, values]) => {
+    values.forEach((value) => {
+      word[value] = key;
+    });
+  });
+  return word.join('');
 }
 
 /**
@@ -178,7 +185,6 @@ function Rectangle(width, height) {
       return this.width * this.height;
     },
   };
-  rectangle.getArea();
   return rectangle;
 }
 
@@ -192,8 +198,8 @@ function Rectangle(width, height) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 /**
@@ -207,8 +213,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return Object.assign(Object.create(proto), JSON.parse(json));
 }
 
 /**
